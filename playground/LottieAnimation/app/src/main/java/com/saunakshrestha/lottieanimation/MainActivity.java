165821957eb0a77232e24109763c6a81e42ef2be
saunakshrestha <sunk.stha@gmail.com>
2022-12-26 22:52:00 +0545

    Lottie animation compledted

---
+++ playground/LottieAnimation/app/src/main/java/com/saunakshrestha/lottieanimation/MainActivity.java
@@ -0,0 +1,21 @@
+package com.saunakshrestha.lottieanimation;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.airbnb.lottie.LottieAnimationView;
+
+public class MainActivity extends AppCompatActivity {
+//    LottieAnimationView laView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+//        LottieAnimationView notes = findViewById(R.id.notes);
+//        laView.setAnimation(R.raw.notes);
+//        notes.playAnimation();
+
+    }
+}
