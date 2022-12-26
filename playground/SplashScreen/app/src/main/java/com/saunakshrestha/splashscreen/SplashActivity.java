e3bf0196a987500c14e99672d3a5ade1a31c1bae
saunakshrestha <sunk.stha@gmail.com>
2022-12-26 18:34:35 +0545

    Splash screen completed

---
+++ playground/SplashScreen/app/src/main/java/com/saunakshrestha/splashscreen/SplashActivity.java
@@ -0,0 +1,25 @@
+package com.saunakshrestha.splashscreen;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent iHome = new Intent(SplashActivity.this, MainActivity.class);
+                startActivity(iHome);
+                finish();
+            }
+        }, 4000);
+    }
+}
