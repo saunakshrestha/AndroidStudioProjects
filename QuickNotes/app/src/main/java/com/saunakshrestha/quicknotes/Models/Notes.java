a4701128809635cd3dcd9e3adf2125cb1118048a
saunakshrestha <sunk.stha@gmail.com>
2022-12-28 19:20:35 +0545

    Designing View after setting database tables

--- QuickNotes/app/src/main/java/com/saunakshrestha/quicknotes/Models/Notes.java
+++ QuickNotes/app/src/main/java/com/saunakshrestha/quicknotes/Models/Notes.java
@@ -4,20 +4,18 @@ import androidx.room.ColumnInfo;
 import androidx.room.Entity;
 import androidx.room.PrimaryKey;
 
-import java.io.Serializable;
 @Entity(tableName = "notes")
-public class Notes implements Serializable {
+public class Notes {
     @PrimaryKey(autoGenerate = true)
     int ID = 0;
-    @ColumnInfo(name="title")
-    String title="";
+    @ColumnInfo(name = "title")
+    String title = "";
     @ColumnInfo(name = "notes")
-    String notes="";
+    String notes = "";
     @ColumnInfo(name = "date")
     String date = "";
     @ColumnInfo(name = "pinned")
-    boolean pinned=false;
-
+    boolean pinned = false;
 
     public int getID() {
         return ID;
