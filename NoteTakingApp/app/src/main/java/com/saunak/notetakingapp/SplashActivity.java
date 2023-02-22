42d566472d78a4efcec93ff90f2aca567cd5fbdf
saunakshrestha <sunk.stha@gmail.com>
2023-02-22 18:04:01 +0545

    finishing the splash activity as not needed onwards

--- NoteTakingApp/app/src/main/java/com/saunak/notetakingapp/SplashActivity.java
+++ NoteTakingApp/app/src/main/java/com/saunak/notetakingapp/SplashActivity.java
@@ -21,6 +21,7 @@ public class SplashActivity extends AppCompatActivity {
             public void run() {
                // After 1 sec , navigating to main activity
                 startActivity(new Intent(SplashActivity.this,MainActivity.class));
+                finish();
 
             }
         },1000);
