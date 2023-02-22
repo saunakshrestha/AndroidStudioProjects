d7d98cafc3fb9f3c0c524d63551381f1a2ee058d
saunakshrestha <sunk.stha@gmail.com>
2023-02-22 21:31:46 +0545

    updated g-services to 4.3.15 & validating email,p,cP

--- NoteTakingApp/app/src/main/java/com/saunak/notetakingapp/CreateAccountActivity.java
+++ NoteTakingApp/app/src/main/java/com/saunak/notetakingapp/CreateAccountActivity.java
@@ -3,14 +3,66 @@ package com.saunak.notetakingapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Patterns;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
 public class CreateAccountActivity extends AppCompatActivity {
+
+    EditText emailEditText,passwordEditText,confirmPasswordEditText;
+    Button createAccountBtn;
+    ProgressBar progressBar;
+    TextView loginBtnTextView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_account);
 
+//link those methods to the ids
+        emailEditText = findViewById(R.id.email_edit_text);
+        passwordEditText = findViewById(R.id.password_edit_text);
+        confirmPasswordEditText = findViewById(R.id.confirm_password_edit_text);
+        createAccountBtn = findViewById(R.id.create_account_btn);
+        progressBar = findViewById(R.id.progress_bar);
+        loginBtnTextView = findViewById(R.id.login_text_view_btn);
+
+        //link all those to xml now lets set onCLickListener and VAlidate the data
+        createAccountBtn.setOnClickListener(v-> createAccount());
+        loginBtnTextView.setOnClickListener(v->finish());
+
+
+
+
 
     }
+    void createAccount(){
+        String email = emailEditText.getText().toString();
+        String password = passwordEditText.getText().toString();
+        String confirmPassword = confirmPasswordEditText.getText().toString();
+        boolean isValidated = validateData(email,password,confirmPassword);
+    }
+//Now lets validate all those data so lets create method for validating those data
+      boolean validateData(String email,String password,String confirmPassword){
+        //validate the data input by user
+        if(!Patterns.EMAIL_ADDRESS.matcher(email).matches()){
+            emailEditText.setError("Email is invalid");
+            return false;
+        }
+        if(password.length()<6){
+            passwordEditText.setError("Password is invalid ");
+            return false;
+        }
+        if(!password.equals(confirmPassword)){
+            confirmPasswordEditText.setError("Password doesnot match");
+            return false;
+        }
+        return true;
+      }
+
+
+
 }
