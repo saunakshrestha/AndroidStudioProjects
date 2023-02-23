a3f88b8380085c351e021b8f7663129c858ea7ab
saunakshrestha <sunk.stha@gmail.com>
2023-02-23 21:12:31 +0545

    Logout button on menu added sucessfully

--- NoteTakingApp/app/src/main/java/com/saunak/notetakingapp/MainActivity.java
+++ NoteTakingApp/app/src/main/java/com/saunak/notetakingapp/MainActivity.java
@@ -6,10 +6,13 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.widget.ImageButton;
+import android.widget.PopupMenu;
 
 import com.firebase.ui.firestore.FirestoreRecyclerOptions;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.firestore.Query;
 
 public class MainActivity extends AppCompatActivity {
@@ -39,6 +42,21 @@ public class MainActivity extends AppCompatActivity {
     }
     void showMenu(){
         //display menu for logout button
+        PopupMenu popupMenu = new PopupMenu(MainActivity.this,menuBtn);
+        popupMenu.getMenu().add("Logout");
+        popupMenu.show();
+        popupMenu.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem menuItem) {
+                if(menuItem.getTitle() == "Logout"){
+                    FirebaseAuth.getInstance().signOut();
+                    startActivity(new Intent(MainActivity.this,LoginActivity.class));
+                    finish();
+                    return true;
+                }
+                return false;
+            }
+        });
 
     }
 
