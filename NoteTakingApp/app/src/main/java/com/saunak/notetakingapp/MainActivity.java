76d180063de13bb4a3c8e391e3d29bbfc386a95f
saunakshrestha <sunk.stha@gmail.com>
2023-02-23 14:44:05 +0545

    NoteDetailsActivity Created

--- NoteTakingApp/app/src/main/java/com/saunak/notetakingapp/MainActivity.java
+++ NoteTakingApp/app/src/main/java/com/saunak/notetakingapp/MainActivity.java
@@ -4,11 +4,17 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
 public class MainActivity extends AppCompatActivity {
 
+    FloatingActionButton addNoteBtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        addNoteBtn = findViewById(R.id.add_note_btn);
+        addNoteBtn.setOnClickListener((v -> ));
     }
 }
---
+++ NoteTakingApp/app/src/main/java/com/saunak/notetakingapp/NoteDetailsActivity.java
@@ -0,0 +1,14 @@
+package com.saunak.notetakingapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class NoteDetailsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_note_details);
+    }
+}
